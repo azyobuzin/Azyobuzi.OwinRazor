ec4a224892fc60606a4c34bfa6402f338b29202c
azyobuzin <azyobuzin@users.sourceforge.jp>
2015-05-03 17:27:07 +0900

    Build-time Razor compilation

---
+++ Azyobuzi.OwinRazor.MSBuild/GenerateRazorCode.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNet.Razor;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Azyobuzi.OwinRazor.MSBuild
+{
+    public class GenerateRazorCode : Task
+    {
+        [Required]
+        public ITaskItem[] Sources { get; set; }
+
+        [Required]
+        public string OutputDirectory { get; set; }
+
+        public string RootNamespace { get; set; }
+
+        [Output]
+        public ITaskItem[] OutputFiles { get; set; }
+
+        private string GetNamespace(string itemSpec)
+        {
+            var s = itemSpec.Split(new[] { '\\', '/' }, StringSplitOptions.RemoveEmptyEntries);
+            if (s.Length == 0) throw new ArgumentException();
+            if (s.Length == 1) return this.RootNamespace;
+
+            var sb = new StringBuilder(this.RootNamespace);
+            for (var i = 0; i < s.Length - 1; i++)
+            {
+                sb.Append('.');
+                sb.Append(s[i]);
+            }
+            return sb.ToString();
+        }
+
+        public override bool Execute()
+        {
+            var success = true;
+            var outputFiles = new List<string>();
+
+            foreach (var source in this.Sources)
+            {
+                var file = new FileInfo(source.GetMetadata("FullPath"));
+                if (!file.Exists)
+                {
+                    this.Log.LogError("{0} is not found.", source.ItemSpec);
+                    success = false;
+                    continue;
+                }
+                var lang = RazorCodeLanguage.GetLanguageByExtension(file.Extension);
+                if (lang == null)
+                {
+                    this.Log.LogError("{0} extension is not supported.", file.Extension);
+                    success = false;
+                    continue;
+                }
+
+                try
+                {
+                    var engine = new RazorTemplateEngine(new RazorEngineHost(lang));
+                    var result = engine.GenerateCode(
+                        file.OpenRead(),
+                        Path.GetFileNameWithoutExtension(file.Name),
+                        GetNamespace(source.ItemSpec),
+                        file.FullName
+                    );
+                    if (result.Success)
+                    {
+                        var outputFile = Path.Combine(this.OutputDirectory, source.ItemSpec);
+                        Directory.CreateDirectory(Path.GetDirectoryName(outputFile));
+                        File.WriteAllText(outputFile, result.GeneratedCode);
+                        outputFiles.Add(outputFile);
+                    }
+                    else
+                    {
+                        foreach (var error in result.ParserErrors)
+                        {
+                            this.Log.LogError(null, null, null, file.FullName,
+                                error.Location.LineIndex, error.Location.CharacterIndex,
+                                0, 0, "{0}", error.Message);
+                        }
+                        success = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    this.Log.LogErrorFromException(ex, true, true, file.FullName);
+                    success = false;
+                }
+            }
+
+            this.OutputFiles = outputFiles.Select(file => new TaskItem(file)).ToArray();
+
+            return success;
+        }
+    }
+}
---
+++ Azyobuzi.OwinRazor.Sample/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Hosting;
+
+namespace Azyobuzi.OwinRazor.Sample
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            const string url = "http://localhost:5000/";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine(url);
+                Console.ReadLine();
+            }
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage().UseWelcomePage();
+        }
+    }
+}
